48c756035a90f382dfaae7201f8826837001fd72
Ederson Ferreira dos Santos <edersonfs@gmail.com>
2026-05-05 17:29:42 -0300

    develop

---
+++ public/controller/work/delete.php
@@ -0,0 +1,70 @@
+<?php
+
+require '../../../vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use app\database\Connection;
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 4));
+$dotenv->load();
+
+$authorization = $_SERVER['HTTP_AUTHORIZATION'];
+
+$token = str_replace('Bearer ', '', $authorization);
+
+include_once '../../../app/database/Connection.php';
+include_once '../../model/work.php';
+
+$conn = new Connection();
+$db = $conn->connect();
+
+try {
+    $decoded = JWT::decode($token, new Key($_SERVER['KEY'], 'HS256'));
+
+    $work = new Work($db);    
+
+    $id = filter_input(INPUT_GET, 'id', FILTER_DEFAULT);    
+
+    if (!$id) {
+        echo json_encode(array("message" => "missing_data_id"));
+        exit;
+    }
+    
+    $work->id = $id;
+    
+    if($work->getById($id)) {
+        if($work->delete($id)) {
+            echo json_encode(array("message" => "success"));
+        } else {
+            echo json_encode(array("message" => "error"));
+        }
+    } else {
+        echo json_encode(array("message" => "record_does_not_exist"));
+    }
+} catch (Throwable $e) {
+    http_response_code(401);
+    die('EXPIRED');
+}
+?>
